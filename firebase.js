1fd859b34279d10b013aaa4e393152474c05c943
Idan <idan85e@gmail.com>
2022-05-12 11:59:05 +0300

    initial structure  for tiktok, includes basic layout and snap animation

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-xWcyIpkzhGvxjqWxi_gVUgj0xzJD8xs",
+  authDomain: "tiktok-clone-90119.firebaseapp.com",
+  projectId: "tiktok-clone-90119",
+  storageBucket: "tiktok-clone-90119.appspot.com",
+  messagingSenderId: "902559475186",
+  appId: "1:902559475186:web:912d0ffe6386b8c741b76b",
+};
